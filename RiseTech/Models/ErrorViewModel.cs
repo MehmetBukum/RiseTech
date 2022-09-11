5a4e8044bfc15a466f10f7d4581d10932c54fd5c
MehmetBukum <mehmetbukum@gmail.com>
2022-09-11 23:35:39 +0300

    Revert "Add project files."
    This reverts commit 9c75a2b4437fdeed272feee3e32ab971e4eead6a.

--- RiseTech/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RiseTech.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
